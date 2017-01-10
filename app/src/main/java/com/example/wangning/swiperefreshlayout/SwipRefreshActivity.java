0d1ac734cc02400bee4120e5966f50d57f1afc79
wangning <wangning@zallds.com>
2017-01-10 18:54:58 +0800

    'aaa'

---
+++ app/src/main/java/com/example/wangning/swiperefreshlayout/SwipRefreshActivity.java
@@ -0,0 +1,18 @@
+package com.example.wangning.swiperefreshlayout;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * file explain
+ *
+ * @author wangning
+ * @version 1.0 2017-01-10
+ * @since JDK 1.8
+ */
+public class SwipRefreshActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
