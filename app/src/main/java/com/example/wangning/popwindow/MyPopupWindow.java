25139d00b3afbd9cb8323850dbf3adfe14610f21
billcarbit <517542596@qq.com>
2017-04-25 23:27:30 +0800

    'aaa'

--- app/src/main/java/com/example/wangning/popwindow/MyPopupWindow.java
+++ app/src/main/java/com/example/wangning/popwindow/MyPopupWindow.java
@@ -3,12 +3,17 @@ package com.example.wangning.popwindow;
 import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.PopupWindow;
 
 import com.example.wangning.R;
+import com.example.wangning.utils.AppUtil;
 
 /**
  * file explain
@@ -22,8 +27,10 @@ public class MyPopupWindow {
     private PopupWindow mPopupWindow;
     public MyPopupWindow(Activity activity) {
         mActivity = activity;
-        View popupView = mActivity.getLayoutInflater().inflate(R.layout.popwindow_main, null);
-        mPopupWindow = new PopupWindow(popupView, WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.WRAP_CONTENT, true);
+        View popupView = LayoutInflater.from(mActivity).inflate(R.layout.popwindow_main, null);
+        popupView.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED);
+        int popupHeight =  popupView.getMeasuredHeight();
+        mPopupWindow = new PopupWindow(popupView, AppUtil.dp2px(mActivity,200), popupHeight);
         mPopupWindow.setTouchable(true);
         mPopupWindow.setOutsideTouchable(true);
         mPopupWindow.setBackgroundDrawable(new BitmapDrawable(mActivity.getResources(), (Bitmap) null));
@@ -38,8 +45,20 @@ public class MyPopupWindow {
 
         int[] location = new int[2];
         view.getLocationOnScreen(location);
+        Log.e("AAA", "show:mPopupWindow.getWidth()= "+mPopupWindow.getWidth() );
 
-        mPopupWindow.showAtLocation(view, Gravity.NO_GRAVITY, location[0], location[1]- mPopupWindow.getHeight() * 2);
+        DisplayMetrics metric = new DisplayMetrics();
+        mActivity.getWindowManager().getDefaultDisplay().getMetrics(metric);
+        int width = metric.widthPixels; // 屏幕宽度（像素）
+
+        if(location[0]+ view.getWidth()/2 + mPopupWindow.getWidth() > width){
+            mPopupWindow.showAtLocation(view, Gravity.NO_GRAVITY , location[0]+ view.getWidth()/2 -mPopupWindow.getWidth() , location[1]- mPopupWindow.getHeight()+ view.getHeight()/2);
+
+        }else{
+            mPopupWindow.showAtLocation(view, Gravity.NO_GRAVITY , location[0]+ view.getWidth()/2, location[1]- mPopupWindow.getHeight()+ view.getHeight()/2);
+
+        }
+
 
     }
 
