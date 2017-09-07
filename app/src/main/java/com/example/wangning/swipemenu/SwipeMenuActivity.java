a91709b9854f4fa856d8770c6e845767a119af95
王宁 <wangning@zallds.com>
2017-09-07 18:00:44 +0800

    dsds

---
+++ app/src/main/java/com/example/wangning/swipemenu/SwipeMenuActivity.java
@@ -0,0 +1,22 @@
+package com.example.wangning.swipemenu;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.wangning.R;
+
+/**
+ * file explain
+ *
+ * @author wangning
+ * @version 1.0 2017-09-06
+ * @since JDK 1.8
+ */
+public class SwipeMenuActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_swipemenu);
+
+    }
+}
