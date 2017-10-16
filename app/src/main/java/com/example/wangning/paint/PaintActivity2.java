adc499ae7d922cb137b40edff594ddf1066f79ae
王宁 <wangning@zallds.com>
2017-10-16 18:05:50 +0800

    'dsds'

---
+++ app/src/main/java/com/example/wangning/paint/PaintActivity2.java
@@ -0,0 +1,21 @@
+package com.example.wangning.paint;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.wangning.R;
+
+/**
+ * file explain
+ *
+ * @author wangning
+ * @version 1.0 2017-10-16
+ * @since JDK 1.8
+ */
+public class PaintActivity2 extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_paint);
+    }
+}
---
+++ app/src/main/java/com/example/wangning/paint/PaintImageView.java
@@ -0,0 +1,69 @@
+package com.example.wangning.paint;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Xfermode;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.widget.ImageView;
+
+/**
+ * file explain
+ *
+ * @author wangning
+ * @version 1.0 2017-10-16
+ * @since JDK 1.8
+ */
+public class PaintImageView extends ImageView {
+    private static final String TAG = "PaintImageView";
+    private static final Xfermode[] sModes = {
+            new PorterDuffXfermode(PorterDuff.Mode.CLEAR),
+            new PorterDuffXfermode(PorterDuff.Mode.SRC),
+            new PorterDuffXfermode(PorterDuff.Mode.DST),
+            new PorterDuffXfermode(PorterDuff.Mode.SRC_OVER),
+            new PorterDuffXfermode(PorterDuff.Mode.DST_OVER),
+            new PorterDuffXfermode(PorterDuff.Mode.SRC_IN),
+            new PorterDuffXfermode(PorterDuff.Mode.DST_IN),
+            new PorterDuffXfermode(PorterDuff.Mode.SRC_OUT),
+            new PorterDuffXfermode(PorterDuff.Mode.DST_OUT),
+            new PorterDuffXfermode(PorterDuff.Mode.SRC_ATOP),
+            new PorterDuffXfermode(PorterDuff.Mode.DST_ATOP),
+            new PorterDuffXfermode(PorterDuff.Mode.XOR),
+            new PorterDuffXfermode(PorterDuff.Mode.DARKEN),
+            new PorterDuffXfermode(PorterDuff.Mode.LIGHTEN),
+            new PorterDuffXfermode(PorterDuff.Mode.MULTIPLY),
+            new PorterDuffXfermode(PorterDuff.Mode.SCREEN)
+    };
+
+    public PaintImageView(Context context) {
+        this(context, null);
+    }
+
+    public PaintImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        Log.e(TAG, "onDraw: ");
+        //橡皮擦
+        Paint mEraserPaint = new Paint();
+        mEraserPaint.setAlpha(0);
+        //这个属性是设置paint为橡皮擦重中之重
+        //这是重点
+        //下面这句代码是橡皮擦设置的重点
+        mEraserPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_IN));
+        //上面这句代码是橡皮擦设置的重点（重要的事是不是一定要说三遍）
+        mEraserPaint.setAntiAlias(true);
+        mEraserPaint.setDither(true);
+        mEraserPaint.setStyle(Paint.Style.STROKE);
+        mEraserPaint.setStrokeJoin(Paint.Join.ROUND);
+        mEraserPaint.setStrokeWidth(30);
+        canvas.drawLine(100,100,300,300,mEraserPaint);
+    }
+}
