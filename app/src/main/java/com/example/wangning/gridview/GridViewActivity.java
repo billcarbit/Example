e03f93b24e2dd936e71e922270386a92335695b4
billjoy <517542596@qq.com>
2018-01-04 18:26:11 +0800

    'dsds'

--- app/src/main/java/com/example/wangning/gridview/GridViewActivity.java
+++ app/src/main/java/com/example/wangning/gridview/GridViewActivity.java
@@ -1,12 +1,14 @@
 package com.example.wangning.gridview;
 
 import android.app.Activity;
+import android.database.DataSetObserver;
 import android.os.Bundle;
 
 import android.view.View;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ListAdapter;
 
@@ -19,40 +21,45 @@ import java.util.List;
  * Created by Administrator on 2017/4/25.
  */
 public class GridViewActivity extends Activity {
-    LastRowCenterGridView mGv;
+    GridView mGv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gridview);
-        findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-        List<String> list = new ArrayList<String>();
-        for(int i=0;i<7;i++){
-            list.add("A"+i);
-        }
-      //  LastRowCenterGridView lastRowCenterGridView = (LastRowCenterGridView) findViewById(R.id.lastRowCenterGridView);
-       // lastRowCenterGridView.initData(list,3);
         initView();
         initData();
     }
 
     void initView() {
-       // mGv = (LastRowCenterGridView) findViewById(R.id.gv);
-
+        mGv = (GridView) findViewById(R.id.gv);
     }
 
     void initData() {
-        List<String> stringList = new ArrayList<String>();
-        for (int i = 0; i < 2; i++) {
-            stringList.add("B" + i);
+        List<String> list = new ArrayList<String>();
+        for(int i=0;i<7;i++){
+            list.add("A"+i);
         }
-        mGv.setDataSource(stringList);
-        mGv.createView();
+        mGv.setAdapter(new BaseAdapter() {
+            @Override
+            public int getCount() {
+                return 10;
+            }
+
+            @Override
+            public Object getItem(int position) {
+                return new Object();
+            }
+
+            @Override
+            public long getItemId(int position) {
+                return position;
+            }
+
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                return convertView;
+            }
+        });
     }
 }
