c8baefcf5040fe6cf0d7debd6e66461df8e6aa1f
billjoy <517542596@qq.com>
2018-01-16 18:30:05 +0800

    'dsds'

---
+++ app/src/main/java/com/example/wangning/radio/RadioActivity.java
@@ -0,0 +1,18 @@
+package com.example.wangning.radio;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.wangning.R;
+
+/**
+ * Created by Administrator on 2018/1/16.
+ */
+public class RadioActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_radio_btn);
+    }
+}
