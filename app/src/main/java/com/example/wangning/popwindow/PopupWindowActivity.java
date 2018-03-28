6789c2b6f3f5b3601e94b91d31df91e45e9fe958
billjoy <517542596@qq.com>
2018-03-28 18:30:00 +0800

    'dsds'

--- app/src/main/java/com/example/wangning/popwindow/PopupWindowActivity.java
+++ app/src/main/java/com/example/wangning/popwindow/PopupWindowActivity.java
@@ -1,10 +1,13 @@
 package com.example.wangning.popwindow;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.PopupWindow;
-import android.widget.TextView;
 
 import com.example.wangning.R;
 
@@ -17,25 +20,58 @@ import com.example.wangning.R;
  */
 public class PopupWindowActivity extends Activity {
 
-    ProcessPopupWindow mPopupWindow;
-    private TextView mButton, mButton2;
+    private ProcessPopupWindow mPopupWindow;
+    private Button btnNormalPopupwin;
+    private Button btnAnimatePw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_popwindow);
+        initView();
+        initListener();
+    }
 
-        mButton = (TextView) findViewById(R.id.btn_popupwin);
-        mButton2 = (TextView) findViewById(R.id.btn_popupwin2);
 
-        mButton2.setOnClickListener(new View.OnClickListener() {
+    private void initView() {
+        btnNormalPopupwin = (Button) findViewById(R.id.btn_normal_popupwin);
+        btnAnimatePw = (Button) findViewById(R.id.btn_animate_pw);
+    }
+
+    private void initListener() {
+        btnNormalPopupwin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                View popupView = getLayoutInflater().inflate(R.layout.popwindow_main, null);
+                PopupWindow mPopupWindow = new PopupWindow(popupView, WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.WRAP_CONTENT, true);
+                mPopupWindow.setTouchable(true);
+                mPopupWindow.setOutsideTouchable(true);
+                mPopupWindow.setBackgroundDrawable(new BitmapDrawable(getResources(), (Bitmap) null));
+
+                //mPopupWindow.showAtLocation(view, Gravity.CENTER,0,0);
+                mPopupWindow.showAsDropDown(v);
+                WindowManager.LayoutParams windowLp = getWindow().getAttributes();
+                windowLp.alpha = 0.4f;
+                getWindow().setAttributes(windowLp);
+                mPopupWindow.setOnDismissListener(new PopupWindow.OnDismissListener() {
+                    @Override
+                    public void onDismiss() {
+                        WindowManager.LayoutParams windowLp = getWindow().getAttributes();
+                        windowLp.alpha = 1.0f;
+                        getWindow().setAttributes(windowLp);
+                    }
+                });
+            }
+        });
+
+        btnAnimatePw.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (mPopupWindow == null) {
                     mPopupWindow = new ProcessPopupWindow(PopupWindowActivity.this);
                 }
                 mPopupWindow.show(v);
-                mPopupWindow.startValueAnimator(0.0f,1.0f,2000);
+                mPopupWindow.startValueAnimator(0.0f, 1.0f, 2000);
             }
         });
     }
