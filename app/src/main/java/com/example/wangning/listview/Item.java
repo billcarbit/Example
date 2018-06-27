32b3b1b8a8e63b34198640b2d06370bf6455acef
billjoy <517542596@qq.com>
2018-06-27 18:02:22 +0800

    'dsds'

---
+++ app/src/main/java/com/example/wangning/listview/Item.java
@@ -0,0 +1,25 @@
+package com.example.wangning.listview;
+
+/**
+ * Created by Administrator on 2018/6/22.
+ */
+public class Item {
+    private int type;
+    private String text;
+
+    public int getType() {
+        return type;
+    }
+
+    public void setType(int type) {
+        this.type = type;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
